210b2d0d7c40edd127e5a70a476115784a5d14e1
Johan <johan@macair2020>
2021-05-21 14:14:34 +0200

    added roles

--- src/home.tsx
+++ src/home.tsx
@@ -1,18 +1,23 @@
 import React from "react";
 
 import * as T from "./lib/type";
+import * as U from "./lib/utils";
 
 import { SourceCodeLink } from "./gh-utils";
+
+const listPersonalities: T.MBTI[] = Object.values(T.MBTI)
+  .filter((x) => typeof x !== "string")
+  .map((x) => x as T.MBTI);
 
 export default () => {
   return (
     <>
       <ul>
-        {Object.values(T.MBTI)
-          .filter((x) => typeof x !== "string")
-          .map((x) => (
-            <li>{T.MBTI[x as T.MBTI]}</li>
-          ))}
+        {listPersonalities.map((x) => (
+          <li>
+            {T.MBTI[x as T.MBTI]} - Role: {T.Role[U.getRole(U.toComponent(x))]}
+          </li>
+        ))}
       </ul>
       <SourceCodeLink />
     </>
