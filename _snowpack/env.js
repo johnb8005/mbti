8c5fb75c9cdf3291d5722ddb0f76842c495cb9fc
johnb8005 <johnb8005@users.noreply.github.com>
2021-05-21 17:25:28 +0000

    deploy: 8ab1b3cccbe2e1d74b03e18877066108847b8ae1

--- _snowpack/env.js
+++ _snowpack/env.js
@@ -1,4 +1,4 @@
-export const SNOWPACK_PUBLIC_GIT_SHA = "2c64756b5ef53540236e05c0f3df70e425a453f7";
+export const SNOWPACK_PUBLIC_GIT_SHA = "8ab1b3cccbe2e1d74b03e18877066108847b8ae1";
 export const SNOWPACK_PUBLIC_URL = "/mbti";
 export const MODE = "production";
 export const NODE_ENV = "production";
--- src/detail/content.js
+++ src/detail/content.js
@@ -13,20 +13,22 @@ const Content = ({mbtiId}) => {
   }, /* @__PURE__ */ React.createElement("img", {
     height: 300,
     src: toImgPath(mbtiId)
-  }), /* @__PURE__ */ React.createElement("br", null), /* @__PURE__ */ React.createElement("small", null, "Image from", " ", /* @__PURE__ */ React.createElement("a", {
+  }), /* @__PURE__ */ React.createElement("br", null), /* @__PURE__ */ React.createElement("small", {
+    className: "float-right"
+  }, "Image from", " ", /* @__PURE__ */ React.createElement("a", {
     href: "https://16personalities.com"
   }, "16Personalities.com"))), /* @__PURE__ */ React.createElement("ul", {
     className: "list list-unstyled"
-  }, /* @__PURE__ */ React.createElement("li", null, "Role: ", /* @__PURE__ */ React.createElement("code", null, T.Role[role])), /* @__PURE__ */ React.createElement("li", null, "Cognitive Function: ")), /* @__PURE__ */ React.createElement("h5", null, "Cognitive Functions/Type Dynamics"), /* @__PURE__ */ React.createElement("p", null, /* @__PURE__ */ React.createElement("small", null, "see", " ", /* @__PURE__ */ React.createElement("a", {
-    href: "https://en.wikipedia.org/wiki/Jungian_cognitive_functions#Isabel_Myers"
-  }, /* @__PURE__ */ React.createElement("i", {
-    className: "fa fa-link"
-  })))), /* @__PURE__ */ React.createElement("ul", null, [
+  }, /* @__PURE__ */ React.createElement("li", null, "Role: ", /* @__PURE__ */ React.createElement("code", null, T.Role[role]))), /* @__PURE__ */ React.createElement("h5", null, "Cognitive Functions/Type Dynamics"), /* @__PURE__ */ React.createElement("ul", null, [
     ["Dominant", U.toDominant(cps)],
     ["Auxiliary", U.toAuxiliary(cps)],
     ["Tertiary", U.toTertiary(cps)],
     ["Inferior", U.toInferior(cps)]
-  ].map(([label, c]) => /* @__PURE__ */ React.createElement("li", null, label, ": ", /* @__PURE__ */ React.createElement("code", null, T.CognitiveFunction[c])))), /* @__PURE__ */ React.createElement("h5", null, "Romantic Partners"), /* @__PURE__ */ React.createElement("ul", {
+  ].map(([label, c]) => /* @__PURE__ */ React.createElement("li", null, label, ": ", /* @__PURE__ */ React.createElement("code", null, T.CognitiveFunction[c])))), /* @__PURE__ */ React.createElement("p", null, /* @__PURE__ */ React.createElement("small", null, "see", " ", /* @__PURE__ */ React.createElement("a", {
+    href: "https://en.wikipedia.org/wiki/Jungian_cognitive_functions#Isabel_Myers"
+  }, /* @__PURE__ */ React.createElement("i", {
+    className: "fa fa-link"
+  })))), /* @__PURE__ */ React.createElement("h5", null, "Romantic Partners"), /* @__PURE__ */ React.createElement("ul", {
     className: "list-group"
   }, U.findRomanticPartners(cps).map((x) => /* @__PURE__ */ React.createElement(UIUtils.PersonalitySmallRow, {
     mbti: U.fromComponent(x)
