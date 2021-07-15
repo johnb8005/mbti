8c5251fb513a4a1a57d8856fc6d4fa855e9468ce
johnb8005 <johnb8005@users.noreply.github.com>
2021-07-15 08:38:18 +0000

    deploy: 0e2a06e00f58ab309f70090462c215509a33931d

--- _snowpack/env.js
+++ _snowpack/env.js
@@ -1,4 +1,4 @@
-export const SNOWPACK_PUBLIC_GIT_SHA = "57018a2165655dbd995a758ee27f0ba2909c7c1a";
+export const SNOWPACK_PUBLIC_GIT_SHA = "0e2a06e00f58ab309f70090462c215509a33931d";
 export const SNOWPACK_PUBLIC_URL = "/mbti";
 export const MODE = "production";
 export const NODE_ENV = "production";
